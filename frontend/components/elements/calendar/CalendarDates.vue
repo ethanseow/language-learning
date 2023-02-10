236ca48173c2b674720b197cf12a8537e86d1377
Ethan Seow <eseow6088@bths.edu>
2023-02-10 00:08:26 -0500

    created table for calendar

--- frontend/components/elements/calendar/CalendarDates.vue
+++ frontend/components/elements/calendar/CalendarDates.vue
@@ -2,10 +2,26 @@
 	<div class="bg-black m-auto mt h-2/3 w-1/2 justify-center">
 		<div>{{ month }} {{ 2023 }}</div>
 		<ArrowIcon class="rotate w-[15px] h-[15px]" />
+		<table class="w-full">
+			<tr>
+				<th class="text-white" v-for="day in daysOfTheWeek">
+					{{ day }}
+				</th>
+			</tr>
+			<tr v-for=""></tr>
+		</table>
 	</div>
 </template>
+<style scoped>
+table,
+tr,
+th {
+	border: 1px white solid;
+}
+</style>
 
 <script setup lang="ts">
+const daysOfTheWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const months = [
 	"January",
 	"February",
@@ -23,6 +39,11 @@ const months = [
 const monthIndex = ref(1);
 const month = computed(() => {
 	return months[monthIndex.value];
+});
+const today = new Date();
+// 0 is sunday - 6 is saturday
+const weekIndex = computed(() => {
+	return today.getDay();
 });
 </script>
 
