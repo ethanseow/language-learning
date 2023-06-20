8f19004902e0ba69e670d87d3da0b64acc1c931d
Ethan Seow <eseow6088@bths.edu>
2023-06-19 23:17:27 -0400

    modal is finished

--- frontend/components/layout/Modal.vue
+++ frontend/components/layout/Modal.vue
@@ -1,9 +1,104 @@
+<!---
 <template>
-	<div>
+	<div
+		id="modal"
+		class="w-screen bg-background opacity-90 h-screen fixed top-0 left-0 z-[0]"
+	>
+		<div class="opacity-100">
+			<slot> </slot>
+		</div>
+	</div>
+
+	<div
+		class="fixed w-screen h-screen z-[1] top-0 left-0 flex flex-col opacity-100"
+	>
 		<slot> </slot>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const modal = ref();
+const props = defineProps<{
+	closeModalFunc: Function;
+}>();
+const modalCloseEvent = (event: MouseEvent) => {
+	console.log("modalCloseEvent is called");
+	const clickedElement = event.target as HTMLElement;
+	const wrapperElement = event.currentTarget as HTMLElement;
+
+	if (clickedElement === wrapperElement) {
+		console.log("modalCloseEvent - closing modal");
+		props.closeModalFunc();
+	}
+};
+onMounted(() => {
+	console.log("onMounted running in bg");
+	document
+		.getElementById("modal")
+		?.addEventListener("click", modalCloseEvent);
+});
+onUnmounted(() => {
+	document
+		.getElementById("modal")
+		?.removeEventListener("click", modalCloseEvent);
+});
+</script>
 
 <style scoped></style>
+-->
+<template>
+	<div ref="modalRef" class="modal">
+		<div class="modal-content">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	closeModalFunc: Function;
+}>();
+
+const modalRef = ref<HTMLElement | null>(null);
+
+const handleBackdropClick = (event: MouseEvent) => {
+	console.log("calling backdrop");
+	const modalElement = modalRef.value;
+	console.log(modalElement);
+	console.log(event.target);
+	if (modalElement && modalElement == event.target) {
+		props.closeModalFunc();
+	}
+};
+
+onMounted(() => {
+	setTimeout(() => {
+		document.addEventListener("click", handleBackdropClick);
+	}, 0);
+});
+
+onUnmounted(() => {
+	document.addEventListener("click", handleBackdropClick);
+});
+</script>
+
+<style scoped>
+.modal {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	backdrop-filter: blur(5px);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.modal-content {
+	background-color: white;
+	padding: 20px;
+	border-radius: 4px;
+}
+</style>
---
+++ frontend/pages/test2.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<Modal v-if="open" :close-modal-func="closeModal">
+			<div class="text-white bg-red-500 w-96 h-96 m-auto">
+				This is a modal
+			</div>
+		</Modal>
+		<button @click="openModal" class="text-white bg-blue-700 p-4">
+			Click me
+		</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+const open = ref(false);
+const closeModal = () => {
+	open.value = false;
+};
+const openModal = () => {
+	open.value = true;
+};
+</script>
+
+<style scoped></style>
